24c4bf3feb9998282f7a5da8baf22f10ccc7d2e3
Kraity <kraits@qq.com>
2022-10-19 17:02:25 +0800

    perf: improve the Radar for Kat by `table switch`
    Signed-off-by: Kraity <kraits@qq.com>

--- kat/src/main/java/plus/kat/kernel/Radar.java
+++ kat/src/main/java/plus/kat/kernel/Radar.java
@@ -115,176 +115,235 @@ public class Radar implements Solver {
         Radar:
         // decode kat stream
         while (true) {
+            // table switch
             byte b = r.next();
-            switch (b >> 3) {
-                case 0xF: {
-                    switch (b) {
-                        case 0x7B: {
-                            if (p.attach(s, a)) {
-                                s.clean();
-                                a.clean();
-                            } else {
-                                s.clean();
-                                a.clean();
-                                washing(r);
-                            }
+            switch (b) {
+                case 0x7B: {
+                    if (p.attach(s, a)) {
+                        s.clean();
+                        a.clean();
+                    } else {
+                        s.clean();
+                        a.clean();
+                        washing(r);
+                    }
+                    c = s;
+                    continue;
+                }
+                case 0x3A: {
+                    if (c.isSpace()) {
+                        c = a;
+                        continue;
+                    } else {
+                        throw new SolverCrash(
+                            a.getClass() + " is not empty <"
+                                + a + "> and `:` can't be repeated"
+                        );
+                    }
+                }
+                case 0x7D: {
+                    if (c.isSpace() &&
+                        s.isEmpty()) {
+                        if (p.detach()) {
                             c = s;
                             continue;
-                        }
-                        case 0x7D: {
-                            if (c.isSpace() &&
-                                s.isEmpty()) {
-                                if (p.detach()) {
-                                    c = s;
-                                    continue;
-                                } else break Radar;
-                            }
-                            throw new SolverCrash(
-                                "`" + b + "` can't be in " + c.getClass()
-                            );
-                        }
-                        case 0x7F: {
-                            throw new SolverCrash(
-                                "Control character `" + b + "` can't be here"
-                            );
-                        }
-                        default: {
-                            c.concat(b);
-                        }
+                        } else break Radar;
                     }
-                    continue;
+                    throw new SolverCrash(
+                        "`" + b + "` can't be in " + c.getClass()
+                    );
                 }
-                case 0xB: {
-                    if (b != 0x5E) {
-                        c.concat(b);
-                    } else {
-                        escape(c, r);
-                    }
-                    continue;
-                }
-                case 0x7: {
-                    if (b != 0x3A) {
-                        c.concat(b);
-                    } else {
-                        if (c.isSpace()) {
-                            c = a;
-                            continue;
-                        } else {
-                            throw new SolverCrash(
-                                a.getClass() + " is not empty <"
-                                    + a + "> and `:` can't be repeated"
-                            );
-                        }
-                    }
-                    continue;
-                }
-                case 0x6:
-                case 0x8:
-                case 0x9:
-                case 0xA:
-                case 0xC:
-                case 0xD:
-                case 0xE:
+                case 0x21:
+                case 0x22:
+                case 0x24:
+                case 0x25:
+                case 0x26:
+                case 0x27:
+                case 0x2A:
+                case 0x2B:
+                case 0x2C:
+                case 0x2D:
+                case 0x2E:
+                case 0x2F:
+                case 0x30:
+                case 0x31:
+                case 0x32:
+                case 0x33:
+                case 0x34:
+                case 0x35:
+                case 0x36:
+                case 0x37:
+                case 0x38:
+                case 0x39:
+                case 0x3B:
+                case 0x3C:
+                case 0x3D:
+                case 0x3E:
+                case 0x3F:
+                case 0x40:
+                case 0x41:
+                case 0x42:
+                case 0x43:
+                case 0x44:
+                case 0x45:
+                case 0x46:
+                case 0x47:
+                case 0x48:
+                case 0x49:
+                case 0x4A:
+                case 0x4B:
+                case 0x4C:
+                case 0x4D:
+                case 0x4E:
+                case 0x4F:
+                case 0x50:
+                case 0x51:
+                case 0x52:
+                case 0x53:
+                case 0x54:
+                case 0x55:
+                case 0x56:
+                case 0x57:
+                case 0x58:
+                case 0x59:
+                case 0x5A:
+                case 0x5B:
+                case 0x5C:
+                case 0x5D:
+                case 0x5F:
+                case 0x60:
+                case 0x61:
+                case 0x62:
+                case 0x63:
+                case 0x64:
+                case 0x65:
+                case 0x66:
+                case 0x67:
+                case 0x68:
+                case 0x69:
+                case 0x6A:
+                case 0x6B:
+                case 0x6C:
+                case 0x6D:
+                case 0x6E:
+                case 0x6F:
+                case 0x70:
+                case 0x71:
+                case 0x72:
+                case 0x73:
+                case 0x74:
+                case 0x75:
+                case 0x76:
+                case 0x77:
+                case 0x78:
+                case 0x79:
+                case 0x7A:
+                case 0x7C:
+                case 0x7E:
                 default: {
                     c.concat(b);
                     continue;
                 }
-                case 0x0:
-                case 0x1:
-                case 0x2:
-                case 0x3: {
-                    switch (b) {
-                        case 0x09:
-                        case 0x0A:
-                        case 0x0D: {
-                            if (c.isEmpty()) {
+                case 0x00:
+                case 0x01:
+                case 0x02:
+                case 0x03:
+                case 0x04:
+                case 0x05:
+                case 0x06:
+                case 0x07:
+                case 0x08:
+                case 0x0B:
+                case 0x0C:
+                case 0x0E:
+                case 0x0F:
+                case 0x10:
+                case 0x11:
+                case 0x12:
+                case 0x13:
+                case 0x14:
+                case 0x15:
+                case 0x16:
+                case 0x17:
+                case 0x18:
+                case 0x19:
+                case 0x1A:
+                case 0x1B:
+                case 0x1C:
+                case 0x1D:
+                case 0x1E:
+                case 0x1F:
+                case 0x7F: {
+                    throw new SolverCrash(
+                        "Control character `"
+                            + b + "` can't be here"
+                    );
+                }
+                case 0x5E: {
+                    escape(c, r);
+                    continue;
+                }
+                case 0x29: {
+                    throw new SolverCrash(
+                        "Close parentheses can't be here"
+                    );
+                }
+                case 0x09:
+                case 0x0A:
+                case 0x0D:
+                case 0x20: {
+                    if (c.isEmpty()) {
+                        if (r.also()) {
+                            continue;
+                        } else return;
+                    }
+                    throw new SolverCrash(
+                        c.getClass() + " is not empty <"
+                            + c + "> and whitespace can't be here"
+                    );
+                }
+                case 0x28: {
+                    while (true) {
+                        switch (b = r.next()) {
+                            case 0x5E: {
+                                escape(v, r);
+                                continue;
+                            }
+                            case 0x29: {
+                                p.accept(
+                                    s, a, v
+                                );
+                                s.clean();
+                                a.clean();
+                                v.clean();
                                 if (r.also()) {
-                                    continue;
+                                    c = s;
+                                    continue Radar;
                                 } else return;
                             }
-                            throw new SolverCrash(
-                                c.getClass() + " is not empty <"
-                                    + c + "> and whitespace can't be here"
-                            );
+                            default: {
+                                v.concat(b);
+                            }
+                        }
+                    }
+                }
+                case 0x23: {
+                    if (c.isSpace() &&
+                        s.isEmpty()) {
+                        while (true) {
+                            switch (r.next()) {
+                                case 0x0A:
+                                case 0x0D:
+                                case 0x23: {
+                                    continue Radar;
+                                }
+                            }
                         }
                     }
                     throw new SolverCrash(
-                        "Control character `" + b + "` can't be here"
+                        c.getClass() + " is not empty <"
+                            + c + "> and comment block can't be here"
                     );
-                }
-                case 0x4: {
-                    switch (b) {
-                        case 0x20: {
-                            if (c.isEmpty()) {
-                                if (r.also()) {
-                                    continue;
-                                } else return;
-                            }
-                            throw new SolverCrash(
-                                c.getClass() + " is not empty <"
-                                    + c + "> and space character can't be here"
-                            );
-                        }
-                        case 0x23: {
-                            if (c.isSpace() &&
-                                s.isEmpty()) {
-                                while (true) {
-                                    switch (r.next()) {
-                                        case 0x0A:
-                                        case 0x0D:
-                                        case 0x23: {
-                                            continue Radar;
-                                        }
-                                    }
-                                }
-                            }
-                            throw new SolverCrash(
-                                c.getClass() + " is not empty <"
-                                    + c + "> and comment block can't be here"
-                            );
-                        }
-                        default: {
-                            c.concat(b);
-                        }
-                    }
-                    continue;
-                }
-                case 0x5: {
-                    switch (b) {
-                        case 0x28: {
-                            while (true) {
-                                switch (b = r.next()) {
-                                    case 0x5E: {
-                                        escape(v, r);
-                                        continue;
-                                    }
-                                    case 0x29: {
-                                        p.accept(
-                                            s, a, v
-                                        );
-                                        s.clean();
-                                        a.clean();
-                                        v.clean();
-                                        if (r.also()) {
-                                            c = s;
-                                            continue Radar;
-                                        } else return;
-                                    }
-                                    default: {
-                                        v.concat(b);
-                                    }
-                                }
-                            }
-                        }
-                        case 0x29: {
-                            throw new SolverCrash(
-                                "Close parentheses can't be here"
-                            );
-                        }
-                        default: {
-                            c.concat(b);
-                        }
-                    }
                 }
             }
         }
